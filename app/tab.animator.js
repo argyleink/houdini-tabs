9c3873d0367fb1ba53dabe526059b766fe16de4c
Adam Argyle <atom@argyleink.com>
2019-08-20 21:59:26 -0700

    simplifying, removing console log

--- app/tab.animator.js
+++ app/tab.animator.js
@@ -6,14 +6,10 @@ registerAnimator('current-tab', class {
   }
 
   animate(currentTime, effect) {
-    const scrollOffset = currentTime
-    const delta = Math.abs(scrollOffset - this._offset)
+    const delta = Math.abs(currentTime - this._offset)
     
     // bug, at 1 we disable the animation!
     const progress = Math.min(delta / (this._width * this._sections), 0.999)
-
-    if (progress > 0 && progress < 1)
-      console.log(`Article active at ${scrollOffset} with (${this._offset}, ${this._width}) => ${progress}`)
 
     effect.localTime = progress * 100
   }
